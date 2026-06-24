2f7792c8194506f048aa5099cb8a0c74be8cdd9c
roi7fri-arch <roi7fri@gmail.com>
2026-06-24 11:58:04 +0300

    Add files via upload

---
+++ namespaces/main_uts.c
@@ -0,0 +1,106 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <fcntl.h>              /* Obtain O_* constant definitions */
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+
+struct params {
+    int fd[2];
+    char **argv;
+};
+
+static void parse_args(int argc, char **argv,
+                       struct params *params)
+{
+#define NEXT_ARG() do { argc--; argv++; } while (0)
+    // Skip binary path
+    NEXT_ARG();
+
+    if (argc < 1) {
+        printf("Nothing to do!\n");
+        exit(0);
+    }
+
+    params->argv = argv;
+#undef NEXT_ARG
+}
+
+#define STACKSIZE (1024*1024)
+static char cmd_stack[STACKSIZE];
+
+
+void await_setup(int pipe)
+{
+    // We're done once we read something from the pipe.
+    char buf[2];
+    if (read(pipe, buf, 2) != 2)
+        printf("Failed to read from pipe: %m\n");
+}
+
+
+static int cmd_exec(void *arg)
+{
+    // Kill the cmd process if the isolate process dies.
+    if (prctl(PR_SET_PDEATHSIG, SIGKILL))
+        printf("cannot PR_SET_PDEATHSIG for child process: %m\n");
+
+    struct params *params = (struct params*) arg;
+    // Wait for 'setup done' signal from the main process.
+    await_setup(params->fd[0]);
+
+    char **argv = params->argv;
+    char *cmd = argv[0];
+    printf("===========%s============\n", cmd);
+
+    if (execvp(cmd, argv) == -1)
+        printf("Failed to exec %s: %m\n", cmd);
+
+    printf("¯\\_(ツ)_/¯");
+    return 1;
+}
+
+
+
+int main(int argc, char **argv)
+{
+
+    struct params params;
+    memset(&params, 0, sizeof(struct params));
+    parse_args(argc, argv, &params);
+
+    // Create pipe to communicate between main and command process.
+    if (pipe(params.fd) < 0)
+        printf("Failed to create pipe: %m");
+
+    // Clone command process.
+    int clone_flags = SIGCHLD | CLONE_NEWUTS ;
+    int cmd_pid = clone(cmd_exec, cmd_stack + STACKSIZE, clone_flags, &params);
+
+    if (cmd_pid < 0)
+        printf("Failed to clone: %m\n");
+
+    // Get the writable end of the pipe.
+    int pipe = params.fd[1];
+
+    // Some namespace setup will take place here ...
+
+    // Signal to the command process we're done with setup.
+    if (write(pipe, "OK", 2) != 2)
+        printf("Failed to write to pipe: %m");
+    if (close(pipe))
+        printf("Failed to close pipe: %m");
+
+    if (waitpid(cmd_pid, NULL, 0) == -1)
+        printf("Failed to wait pid %d: %m\n", cmd_pid);
+
+    return 0;
+}
